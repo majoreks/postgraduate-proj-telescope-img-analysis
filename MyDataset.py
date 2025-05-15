b608736e0ae9f16392934d743153639c2297782d
Raul Onrubia <ronrubii@gmail.com>
2025-05-15 02:33:32 +0200

    MyDataset
    labels still to be "cropped"

---
+++ MyDataset.py
@@ -0,0 +1,74 @@
+import os
+import getStridedVector
+from torch.utils.data import Dataset
+from astropy.io import fits
+import numpy as np
+
+
+class MyDataset(Dataset):
+    def __init__(self, images_path, labels_path, transform=None, Npixels=512):
+        super().__init__()
+        self.images_path = images_path
+        self.labels_path = labels_path
+        self.transform = transform
+
+        self.images_list = os.listdir(self.images_path)
+        self.labels_list = os.listdir(self.labels_path)
+
+        images_list = [image.split("_V_")[0] for image in self.images_list]
+        labels_list = [label.split("_V_")[0] for label in self.labels_list]
+
+        combined_list = [image for image in images_list if image in labels_list]
+
+        image_str = "_V_imc.fits.gz"
+        label_str = "_V_imc_trl.dat"
+
+        self.images_list = [image + image_str for image in combined_list]
+        self.labels_list = [label + label_str for label in combined_list]
+
+        length = 4108
+        width = 4096
+
+        lentgh_positions = getStridedVector(length, Npixels)
+        width_positions = getStridedVector(width, Npixels)
+
+        self.Nlength = len(lentgh_positions)
+        self.Nwidth = len(width_positions)
+        self.Nsubimages = self.Nlength * self.Nwidth
+
+        lenmesh, lonmesh = np.meshgrid(range(self.Nlength), range(self.Nwidth))
+        posindexes = np.array([lenmesh.flatten(), lonmesh.flatten()]).T
+
+        self.coordinates = []
+
+        for ii in range(posindexes.shape[0]):
+            x = lentgh_positions[posindexes[ii][0]]
+            y = width_positions[posindexes[ii][1]]
+            elem = [x[0],  x[1], y[0], y[1]]
+            self.coordinates.append(elem)
+
+    def __len__(self):
+        return len(self.images_list)*self.Nsubimages
+    
+    def __getitem__(self, idx):
+        image_idx = idx // self.Nsubimages
+        subimage_idx = idx % self.Nsubimages
+
+        image_path = os.path.join(self.images_path, self.images_list[image_idx])
+        label_path = os.path.join(self.labels_path, self.labels_list[image_idx])
+
+        coords = self.coordinates[subimage_idx]
+
+        # Load the image and label data
+        with fits.open(image_path) as hdul:
+            image_data = hdul[0].data[coords[0]:coords[1], coords[2]:coords[3]]
+            # image_data_cut = image_data
+
+        with open(label_path, 'r') as f:
+            labels_data = f.readlines()
+            # still to be done
+
+    
+        return image_data, labels_data
+        # return image_data, image_data_cut
+        
---
+++ getStridedVector.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def getStridedVector(length, Num_pixels):
+
+    Nheight = np.ceil(length / Num_pixels)
+    excessheight = int(( - length % Num_pixels ) % Num_pixels)
+    heightstride = int(excessheight // (Nheight-1))
+    pos = []
+
+    for ii in range(int(Nheight)):
+        if ii == 0:
+            elem = [0, Num_pixels-1]
+        elif ii == Nheight-1:
+            elem = [length-Num_pixels, length-1]
+        else:
+            origin = pos[-1][1] - heightstride +1
+            elem = [origin, origin+Num_pixels-1]
+        pos.append(elem)
+        # print(f"element {ii} : {pos[ii][0]} - {pos[ii][1]} \t Npixels = {pos[ii][1]-pos[ii][0]+1} \t pos has {len(pos)} elements")
+    return pos
