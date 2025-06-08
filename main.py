aa7321fac1eda1d84fbbdeb4da954aeeed0b8829
majoreks <s.majorek.konta@gmail.com>
2025-06-09 00:31:31 +0200

    calculate loss

--- main.py
+++ main.py
@@ -32,19 +32,13 @@ def train_single_epoch(model, images, targets, optimizer, device):
     return predictions, loss_dict
 
 
-def eval_single_epoch(model, val_loader):
-    # accs, losses = [], []
-    # with torch.no_grad():
-    #     model.eval()
-    #     for x, y in val_loader:
-    #         x, y = x.to(device), y.to(device)
-    #         y_ = model(x)
-    #         loss = F.cross_entropy(y_, y)
-    #         acc = accuracy(y, y_)
-    #         losses.append(loss.item())
-    #         accs.append(acc.item())
-    # return np.mean(losses), np.mean(accs)
-    pass
+def eval_single_epoch(model, images, targets):
+    images = [img.to(device) for img in images]
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+    predictions, loss_dict = model(images, targets)
+
+    return predictions, loss_dict
 
 
 def train_model(config, tempdir):
@@ -62,19 +56,27 @@ def train_model(config, tempdir):
     model = model.train()
     optimizer = torch.optim.Adam(list(model.backbone.parameters()) + list(model.roi_heads.box_predictor.parameters()), lr=1e-4, weight_decay=0.001)
 
-    loss_history = defaultdict(list)
+    train_loss_history = defaultdict(list)
+    eval_loss_history = defaultdict(list)
     for epoch in range(config['epochs']):
         print(f"\nEpoch {epoch+1}/{config['epochs']}")
 
-        for _, (images, targets) in tqdm(enumerate(train_loader), total=len(train_loader), desc=f"Epoch {epoch+1}"):
-            model.train()
+        for _, (images, targets) in tqdm(enumerate(train_loader), total=len(train_loader), desc=f"train | epoch {epoch+1}"):
             predictions, loss_dict = train_single_epoch(model, images, targets, optimizer, device)
 
             for k, v in loss_dict.items():
-                loss_history[k].append(v.item())
+                train_loss_history[k].append(v.item())
+
+        with torch.no_grad():
+            for _, (images, targets) in tqdm(enumerate(val_loader), total=len(val_loader), desc=f"eval | epoch {epoch+1}"):
+                predictions, loss_dict = eval_single_epoch(model, images, targets)
+
+                for k, v in loss_dict.items():
+                    eval_loss_history[k].append(v.item())
 
     save_model(model)
-    plot_losses(loss_history, fname="train_loss.png", save_plot=True)
+    plot_losses(train_loss_history, fname="train_loss.png", save_plot=True)
+    plot_losses(eval_loss_history, fname="eval_loss.png", save_plot=True)
 
 def inference(path, tempdir):
     '''print('inference',path)
@@ -166,7 +168,7 @@ def main() -> None:
 
     config = {
         "lr": 1e-3,
-        "batch_size": 8,
+        "batch_size": 6,
         "epochs": 15,
         "train_data_path": "data_full"
     }
