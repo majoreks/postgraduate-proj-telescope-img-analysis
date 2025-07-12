a2220a3115a1927bbc56db1277a834825bf129f5
Gerard Moyano <cfiedrichgauss@gmail.com>
2025-07-12 17:45:27 +0200

    Added the wrapper needed for the sweep agent to work

---
+++ hyperparam_search/sweep_wrapper.py
@@ -0,0 +1,14 @@
+import wandb
+from train.traineval import train_experiment
+from logger.logger import Logger
+
+def sweep_wrapper_factory(base_config, sweep_config, task, dev, device, tempdir):
+    def sweep_wrapper():   
+        run = wandb.init(
+            project="postgraduate-sat-object-detection",
+            config=sweep_config,
+            tags=["hyperparam-sweep"]
+        )
+        sweep_params = dict(wandb.config)                 
+        train_experiment(base_config, tempdir, task, dev, device, sweep_params)
+    return sweep_wrapper
