f42dfedd3521a0655cb911547be79364117f4f01
Gerard Moyano <cfiedrichgauss@gmail.com>
2025-07-12 17:42:41 +0200

    Mode experiment added

--- config/mode.py
+++ config/mode.py
@@ -3,6 +3,7 @@ from enum import Enum
 class Mode(Enum):
     TRAIN = "train"
     INFER = "infer"
+    EXPERIMENT = "experiment"
 
     def __str__(self):
         return self.value  # For argparse to print readable choices
